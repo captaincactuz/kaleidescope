ac1c33d6ae6f1f4e8e2c95ae440f9c7f01a1997f
saintwilbur <saintwilbur@gmail.com>
2015-03-12 20:35:29 -0400

    manvids threejs kaleidescope smashed up against johnnyfive express

--- js/proto/script.js
+++ js/proto/script.js
@@ -1,40 +1,56 @@
-var app = require('http').createServer(handler),
-     io = require('socket.io').listen(app),
+//var app = require('http').createServer(handler),
+var express = require('express'),
      fs = require('fs'),
-   five = require('johnny-five');
-
-app.listen(8080);
-
+     five = require('johnny-five');
+var  app = express();   
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+     //io = require('socket.io').listen(app);
+app.use(express.static(__dirname + '/mrdoob'));
+//app.listen(8080);
+server.listen(8080);
+/*
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
+  //fs.readFile(__dirname + '/mrdoob/examples/webgl.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end('Error loading *index.html: ' + err);
     }
 
     res.writeHead(200);
     res.end(data);
   });
 }
+*/
+
 
 board = new five.Board();
 
 board.on("ready", function() {
   led = new five.Led(13);
-  pot = new five.Sensor({
+  pott = new five.Sensor({
       pin: "A2",
       freq: 250
     });
+  dial = new five.Sensor({
+      pin: "A0",
+      freq: 250
+  });
   board.repl.inject({
-    pot: pot
+    pot: pott,
+    pot: dial
   });
 
   io.sockets.on('connection', function (socket) {
-        pot.on("data", function() {
-      //console.log(this.value, this.raw);
-      socket.emit('message', this.value);
-      //document.getElementById("square").style.color = "500";
+        pott.on("data", function() {
+            //console.log(this.value, this.raw);
+            socket.emit('message', this.value);
+            //document.getElementById("square").style.color = "500";
+        });
+        dial.on("data", function() {
+            socket.emit('dial', this.value);
         });
     socket.on('click', function () {
       led.toggle();
