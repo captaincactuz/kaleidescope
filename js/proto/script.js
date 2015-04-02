88b07af721717f6005c79d7b3201cc4496719060
mtrogers <mtrogers@ufl.edu>
2015-04-02 16:44:01 -0400

    pre mux

--- js/proto/script.js
+++ js/proto/script.js
@@ -29,7 +29,13 @@ function handler (req, res) {
 board = new five.Board();
 
 board.on("ready", function() {
-  led = new five.Led(13);
+  //led = new five.Led(13);
+
+var ctrlA =	new five.Led(13),
+	ctrlB =	new five.Led(12),
+	ctrlC = new five.Led(11);
+  var controlpins = {
+  };
   pott = new five.Sensor({
       pin: "A2",
       freq: 250
@@ -44,10 +50,22 @@ board.on("ready", function() {
   });
 
   io.sockets.on('connection', function (socket) {
+	while(true) {
+	    dial.on("data", function() {
+       	    	socket.emit('dial', this.value);
+            });
+	    //controlpins[0].write(i & 1 ? 1 : 0);
+	    //controlpins[1].write(i & 1 ? 1 : 0);
+	    //controlpins[2].write(i & 1 ? 1 : 0);
+		ctrl.write(i & 1 ? 1 : 0);
+	    if (i++ == 1) {
+		i = 0;
+	    }
+	    
+	}
         pott.on("data", function() {
             //console.log(this.value, this.raw);
             socket.emit('message', this.value);
-            //document.getElementById("square").style.color = "500";
         });
         dial.on("data", function() {
             socket.emit('dial', this.value);
